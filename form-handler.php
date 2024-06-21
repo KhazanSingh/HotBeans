5e40c2a59fb1c859ed6899c12211b02e6e07c9a0
KhazanSingh <132811408+KhazanSingh@users.noreply.github.com>
2024-06-21 10:41:56 +0100

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,12 @@
+<? PHP_BINARY
+
+$name = $_POST["name"];
+$visitor_email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"]
+	
+$email_form = '10singam01@lat16.org.uk';
+
+$email_subject = 'new form submission';
+
+$email_body = 'User Name: $name.\n'.
